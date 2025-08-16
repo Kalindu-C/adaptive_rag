298414bd7ac705f708c94cd6360d4867b45e371e
Kalindu-C <kctchathuranga@gmail.com>
2025-08-16 23:57:47 +0530

    build until main.py

--- README.md
+++ README.md
@@ -5,3 +5,15 @@
                                    - If we want the model to return a Pydantic object, we just need to pass in the desired Pydantic class. 
                                    - For more complex schemas it's very useful to add few-shot examples to the prompt. - add examples to a system message
                                            - etc
+
+
+* retrieval_grader - The retrieval grader acts as a quality control mechanism that evaluates whether retrieved documents are actually relevant to the user’s question. This component is crucial because vector similarity alone doesn’t guarantee relevance — documents might be semantically similar but contextually inappropriate.
+
+This grading step prevents irrelevant documents from contaminating our generation process and triggers web search when local documents are insufficient.
+
+* hallucination_grader - The hallucination grader assesses whether the generated answer is grounded in the provided facts. This is important for ensuring the reliability and accuracy of the information being presented to the user.It compares the generated response against the retrieved documents
+
+* When hallucinations are detected, our system can trigger regeneration or seek additional information, ensuring that users receive accurate and trustworthy responses.
+
+* i got error in this line - answer_grader: RunnableSequence = answer_prompt | structured_llm_grader
+The expression answer_prompt | structured_llm_grader creates a RunnableSerializable, but you're trying to assign it to a variable typed as RunnableSequence
---
+++ graph/graph.py
@@ -0,0 +1,122 @@
+from dotenv import load_dotenv
+from langgraph.graph import END, StateGraph
+
+from graph.chains.answer_grader import answer_grader
+from graph.chains.hallucination_grader import hallucination_grader
+from graph.chains.router import RouteQuery, question_router
+
+from graph.consts import GENERATE, GRADE_DOCUMENTS, RETRIEVE, WEBSEARCH
+from graph.nodes import generate, grade_documents, retrieve, web_search
+
+from graph.state import GraphState
+
+load_dotenv()
+
+def decide_to_generate(state):
+    print("---ASSESS GRADED DOCUMENTS---")
+
+    if state["web_search"]:
+        print(
+            "---DECISION: NOT ALL DOCUMENTS ARE NOT RELEVANT TO QUESTION, INCLUDE WEB SEARCH---"
+        )
+        return WEBSEARCH
+    else:
+        print("---DECISION: GENERATE---")
+        return GENERATE
+
+def grade_generation_grounded_in_documents_and_question(state: GraphState) -> str:
+    print("---CHECK HALLUCINATIONS---")
+    question = state["question"]
+    documents = state["documents"]
+    generation = state["generation"]
+
+    score = hallucination_grader.invoke(
+        {"documents": documents, "generation": generation}
+    )
+
+    if hallucination_grade := score.binary_score:
+        print("---DECISION: GENERATION IS GROUNDED IN DOCUMENTS---")
+        print("---GRADE GENERATION vs QUESTION---")
+        score = answer_grader.invoke({"question": question, "generation": generation})
+        if answer_grade := score.binary_score:
+            print("---DECISION: GENERATION ADDRESSES QUESTION---")
+            return "useful"
+        else:
+            print("---DECISION: GENERATION DOES NOT ADDRESS QUESTION---")
+            return "not useful"
+    else:
+        print("---DECISION: GENERATION IS NOT GROUNDED IN DOCUMENTS, RE-TRY---")
+        return "not supported"
+
+
+def route_question(state: GraphState) -> str:
+    """
+    Route question to web search or RAG.
+
+    Args:
+        state (dict): The current graph state
+
+    Returns:
+        str: Next node to call
+    """
+     
+    print("---ROUTE QUESTION---")
+    question = state["question"]
+    source: RouteQuery = question_router.invoke({"question": question})
+
+    if source.datasource == WEBSEARCH:
+        print("---ROUTE QUESTION TO WEB SEARCH---")
+        return WEBSEARCH
+    elif source.datasource == "vectorstore":
+        print("---ROUTE QUESTION TO RAG---")
+        return RETRIEVE
+
+
+workflow = StateGraph(GraphState)
+
+# graph.add_node(name, value)
+# name: A string representing the unique name of the node within the graph. 
+#The callable (e.g., a function or a LangChain runnable) that the node will 
+# execute when it is called during graph execution.
+
+workflow.add_node(RETRIEVE, retrieve.retrieve) # initially just retrieve
+workflow.add_node(GRADE_DOCUMENTS, grade_documents.grade_documents)
+workflow.add_node(GENERATE, generate.generate)
+workflow.add_node(WEBSEARCH, web_search.web_search)
+
+workflow.set_conditional_entry_point(
+    route_question,
+    {
+        WEBSEARCH: WEBSEARCH,
+        RETRIEVE: RETRIEVE,
+    },
+)
+
+# workflow.set_entry_point(RETRIEVE)
+
+workflow.add_edge(RETRIEVE, GRADE_DOCUMENTS) # why use this ??????
+
+workflow.add_conditional_edges(
+    GRADE_DOCUMENTS,   # what is this parameter ?????
+    decide_to_generate,
+    {
+        WEBSEARCH: WEBSEARCH,
+        GENERATE: GENERATE,
+    },
+)
+
+workflow.add_conditional_edges(
+    GENERATE,
+    grade_generation_grounded_in_documents_and_question,
+    {
+        "not supported": GENERATE,
+        "useful": END,
+        "not useful": WEBSEARCH,
+    },
+)
+workflow.add_edge(WEBSEARCH, GENERATE)   # ????????
+workflow.add_edge(GENERATE, END)   # ????????
+
+app = workflow.compile()
+
+app.get_graph().draw_mermaid_png(output_file_path="graph.png")
